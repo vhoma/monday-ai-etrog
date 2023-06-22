fa9766883cbf79b8c1399047d0f58a01c94ff63b
boriskefer <vb.homa@gmail.com>
2023-06-22 15:04:57 +0300

    endpoint for Vova

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, UploadFile
+from dotenv import dotenv_values
 import uvicorn
 import sys
+import openai
+import requests
 
 app = FastAPI()
+action_items = None
+
 
 @app.post("/recording_ready")
 async def process_recording(file: UploadFile = UploadFile(...)):
@@ -10,13 +15,40 @@ async def process_recording(file: UploadFile = UploadFile(...)):
     # Process the contents of the MP4 file
     # You can replace the print statement with your desired processing logic
     print(f"Received file: {file.filename}, Size: {len(contents)} bytes")
-    return {"message": "Recording processed successfully"}
+    res = process_data(contents)
+    return {"success": True, "transcript": res}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/get_action_items")
+def get_action_items():
+    global action_items
+    if action_items:
+        return {"processing_done": True, "action_items": action_items}
+    else:
+        return {"processing_done": False, "action_items": action_items}
+
+
+def process_data(file_content):
+    transcription = transcribe_audio(file_content)
+    print(f"Transcript: {transcription}")
+    return transcription
+
+
+def transcribe_audio(file_content):
+    tmp_file_path = "./tmp_audio.m4a"
+    with open(tmp_file_path, "wb") as tmp_file:
+        tmp_file.write(file_content)
+
+    with open(tmp_file_path, "rb") as audio_file:
+        transcript = openai.Audio.translate("whisper-1", audio_file)
+
+    return transcript
+
 
 if __name__ == "__main__":
+    config = dotenv_values(".env")
+    openai.api_key = config["openai_api_key"]
+
     if len(sys.argv) > 1:
         port = int(sys.argv[1])
     else:
